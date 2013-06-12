7a370c783c3bd4926ff2a7864bc9f8147ad65d7f
Lisa Yoder <lisa.alane.yoder@gmail.com>
2013-06-12 15:13:03 -0400

    first commit

---
+++ BlogCal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogCal::Application.config.secret_token = '761c1d4e1cc62e86527649949db663aa4f030c7addedddbf06e67b32603568f51431e560a0930994550cdc64a1146605fbefafd4515326bcdac21eef13e1c0a7'
